360a49c0d711ebebe7365880cc5ead28fcd119e3
mshahriarinia <info@mshahriarinia.com>
2014-03-12 18:16:04 -0700

    Added session to haml generated links to maintain restfulness across links whether through submit or links. Made sure user settings are kept even if user goes away and comes back later.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,26 +6,38 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+#if the user has selected any combination of column sorting and restrict-by-rating constraints, 
+#and then the user clicks to see the details of one of the movies (for example), when she clicks 
+#the Back to Movie List on the detail page, the movie listing should “remember” the user’s sorting 
+#and filtering settings from before. params superceds whatever is in session and should update it.
   def index
     @movies = Movie.scoped    # instead of all: in rails 3 all returns array.
     @all_ratings = Movie.ALL_RATINGS
     @ratings_filter_arr = Movie.ALL_RATINGS
 
     logger.info params
-    @order = params[:order]
-    
 
-    if(@order == "title")
-      @movies = Movie.find(:all, :order => "lower(#{@order})")
-    elsif(@order == "release_date")
-      @movies = Movie.find(:all, :order => "#{@order}")
+    if(params[:order])
+      session[:order] = params[:order]
     end
+
+      params[:order] = session[:order]
+      @order = params[:order]
+      if(@order == "title")
+        @movies = Movie.order("lower(#{@order})")
+      elsif(@order == "release_date")
+        @movies = Movie.order("lower(#{@order})")
+      end
 
     #debugger
     if(params[:ratings])
+      session[:ratings] = params[:ratings]
+    end
+
+      params[:ratings] = session[:ratings]
       @ratings_filter_arr = params[:ratings].keys
       @movies = @movies.where("rating in (?)", @ratings_filter_arr) #if params[:ratings]
-    end
+
   end
 
   def new
